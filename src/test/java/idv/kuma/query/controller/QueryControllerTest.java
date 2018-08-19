c7af48a027eaae5eda12250466cb9eafd0bf708b
Yu-Song Syu <kuma@simbo.com.tw>
2018-08-19 21:53:08 +0800

    fix test

--- src/test/java/idv/kuma/query/controller/QueryControllerTest.java
+++ src/test/java/idv/kuma/query/controller/QueryControllerTest.java
@@ -1,41 +1,45 @@
-package idv.kuma.home.controller;
+package idv.kuma.query.controller;
 
+import idv.kuma.query.service.QueryService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultMatcher;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
-import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.when;
 
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @DirtiesContext
-public class WelcomeControllerTest {
+public class QueryControllerTest {
 
     @Autowired
     private TestRestTemplate restTemplate;
 
-    @Test
-    public void testMainPage() throws Exception {
+    @MockBean
+    private QueryService mockedQueryService;
 
-        ResponseEntity<String> entity = this.restTemplate.getForEntity("/", String.class);
+    @Test
+    public void testQueryOK() throws Exception {
+
+        when(mockedQueryService.query(anyString())).thenReturn("haha");
+
+        ResponseEntity<String> entity = this.restTemplate.getForEntity("/query/12345", String.class);
         assertThat(entity.getStatusCode()).isEqualTo(HttpStatus.OK);
-        assertThat(entity.getBody()).contains("Hello World");
+        assertEquals("haha", entity.getBody());
 
     }
-
-
-
 
 }
