84af54633b5cfdec99043bb4c72a14aba2ae53b3
Yu-Song Syu <kuma@simbo.com.tw>
2018-08-19 20:00:28 +0800

    add query controller and contribute controller

---
+++ src/main/java/idv/kuma/controller/QueryController.java
@@ -0,0 +1,16 @@
+package idv.kuma.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class QueryController {
+
+
+    @RequestMapping(value = "/query", method = RequestMethod.GET)
+    public String query() {
+        return "No JP";
+    }
+}
