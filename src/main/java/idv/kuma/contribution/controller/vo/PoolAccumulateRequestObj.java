d29d16de6a0655fd9ef79c53ed290acdbea81ceb
Yu-Song Syu <kuma@simbo.com.tw>
2018-08-22 21:35:10 +0800

    add vo

---
+++ src/main/java/idv/kuma/contribution/controller/vo/PoolAccumulateRequestObj.java
@@ -0,0 +1,21 @@
+package idv.kuma.contribution.controller.vo;
+
+
+
+public class PoolAccumulateRequestObj {
+
+
+//    https://www.baeldung.com/intro-to-project-lombok
+    private long mNum;
+    private int partnerID;
+    private int poolCount;
+    private int[] poolId;
+    private double currencyRate;
+    private double bet;
+    private String location;
+    private String playerID;
+    private String webID;
+    private String gameServerIdentify;
+
+
+}
