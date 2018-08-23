949f02c92c71cecad8722935f229abefd7490e9b
Yu-Song Syu <kuma@simbo.com.tw>
2018-08-23 21:35:35 +0800

    take form

--- src/main/java/idv/kuma/contribution/controller/ContributionController.java
+++ src/main/java/idv/kuma/contribution/controller/ContributionController.java
@@ -1,5 +1,6 @@
 package idv.kuma.contribution.controller;
 
+import idv.kuma.contribution.vo.PoolAccumulateRequestObj;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,10 +9,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class ContributionController {
 
     @RequestMapping(value = "/contribute", method = RequestMethod.POST)
-    public String contribute(){
+    public String contribute(PoolAccumulateRequestObj requestObj) {
 
 
-
-        return "success";
+        return "success " + requestObj.getMNum() + " AAA";
     }
 }
