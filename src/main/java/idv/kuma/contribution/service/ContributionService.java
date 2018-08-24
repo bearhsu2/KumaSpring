fd8a97ad6e5024f072b462181b88327b71830845
Yu-Song Syu <kuma@simbo.com.tw>
2018-08-24 14:19:10 +0800

    make service a component so it can be autowired

--- src/main/java/idv/kuma/contribution/service/ContributionService.java
+++ src/main/java/idv/kuma/contribution/service/ContributionService.java
@@ -2,7 +2,9 @@ package idv.kuma.contribution.service;
 
 
 import idv.kuma.contribution.vo.PoolAccumulateRequestObj;
+import org.springframework.stereotype.Component;
 
+@Component
 public class ContributionService {
 
     public int contribute(PoolAccumulateRequestObj requestObj){
