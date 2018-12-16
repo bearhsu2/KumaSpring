ebe4962a9077f5324789085895a320cf1508fe97
Yu-Song Syu <kuma@simbo.com.tw>
2018-12-16 23:19:12 +0800

    add greeting html page

---
+++ src/main/java/idv/kuma/controller/GreetingController.java
@@ -0,0 +1,17 @@
+package idv.kuma.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class GreetingController {
+
+    @RequestMapping("/greeting")
+    public String greeting() {
+        return "greeting";
+    }
+
+}
