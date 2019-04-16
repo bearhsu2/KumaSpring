10c2d594585e28e1e2f859e0745e746f3df7f0f0
Yu-Song Syu <kuma@simbo.com.tw>
2019-04-16 23:02:46 +0800

    extract data to repository

--- src/main/java/idv/kuma/controller/MainController.java
+++ src/main/java/idv/kuma/controller/MainController.java
@@ -1,32 +1,32 @@
 package idv.kuma.controller;
 
-import idv.kuma.service.CourseService;
+import idv.kuma.repository.CourseRepository;
 import idv.kuma.vo.Course;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Controller
 public class MainController {
 
-    CourseService courseService;
+    CourseRepository courseRepository;
 
 
     @Autowired
-    public MainController(CourseService courseService) {
-        this.courseService = courseService;
+    public MainController(CourseRepository courseRepository) {
+        this.courseRepository = courseRepository;
     }
 
     @GetMapping("/")
     public String index(Model model) {
 
-        List<Course> courses = courseService.getCourses();
+        List<Course> courses = courseRepository.getCourses();
 
         model.addAttribute("courses", courses);
+        
         return "index";
     }
 
--- src/main/java/idv/kuma/repository/CourseRepository.java
+++ src/main/java/idv/kuma/repository/CourseRepository.java
@@ -1,4 +1,4 @@
-package idv.kuma.service;
+package idv.kuma.repository;
 
 import idv.kuma.vo.Course;
 import org.springframework.stereotype.Component;
@@ -7,13 +7,17 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Component
-public class CourseService {
+public class CourseRepository {
 
-    public List<Course> getCourses() {
-        List<Course> courses = new ArrayList<>();
+    private static List<Course> courses = new ArrayList<>();
+
+    static {
         courses.add(new Course("Math", "This is a typical math class", 15D, "N/A"));
         courses.add(new Course("Chinese", "Chinese is important.", 30.5, "Taiwan No. 1!!!"));
         courses.add(new Course("English", "Hello. How are you?", 10D, "I'm fine. Thank you."));
+    }
+
+    public List<Course> getCourses() {
         return courses;
     }
 }
